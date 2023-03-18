5dbf976139b7f2efc5044cf1508105a6af247007
sarainan <sarainan01@gmail.com>
2023-03-18 19:16:39 -0400

    second jdbc

---
+++ src/main/java/JdbcUtils.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JdbcUtils {
+
+    public static Connection connectToDatabase(){
+        DriverManager.getConnection()
+
+    }
+
+
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+import java.sql.DriverManager;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        DriverManager.getConnection();
+
+    }
+}
